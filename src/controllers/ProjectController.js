a6f7315fd50d7ec85915bf81f03134f445cbf9a7
Wlisses Ribeiro <69227825+wlissisrds@users.noreply.github.com>
2022-03-20 14:18:13 -0300

    listando usuarios e seus projetos

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -2,8 +2,40 @@ const knex = require('../database');
 
 
 module.exports = {
-    async index(req, res){
-        const result =  await knex('projects');
-        return res.json(result)
+    async index(req, res, next){
+        try {
+            const {user_id} = req.query;
+            
+            const query = knex('projects');
+
+            if(user_id) {
+                query
+                    .where({user_id: user_id})
+                    .join('users', 'users.id', '=', 'projects.user_id')
+                    .select('projects.*','users.username')
+            }
+
+            const result = await query;
+
+            return res.json(result)
+        } catch (error) {
+            next(error)
+        }
     },
+
+    async create(req, res, next){
+        try {
+            const {title, user_id} = req.body;
+
+            await knex('projects').insert({ 
+                    title: title,
+                    user_id: user_id
+                 })
+
+            return res.send();
+            
+        } catch (error) {
+            next(error)
+        }
+    }
 }
--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,9 @@ routes
     .post("/users", UserController['create'])
     .put("/users/:id", UserController['update'])
     .delete("/users/:id", UserController['delete'])
+    
     .get("/projects",ProjectController['index'])
+    .post("/projects",ProjectController['create'])
 
 
 
